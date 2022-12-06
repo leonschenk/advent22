60e6f3a41dcd672ab507b1bb0b601047aef89538
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2022-12-06 22:24:31 +0000

    Day 6 part 1 (adapted)

--- day6/part1.cpp
+++ day6/part1.cpp
@@ -3,24 +3,28 @@
 #include <sstream>
 #include <deque>
 
+const int distinctCharacters = 4;
+
+inline int getStep(const char* buffer) {
+    for (int j = 1; j < distinctCharacters; j++) {
+        for (int k = 0; k < j; k++) {
+            if (buffer[-k] == buffer[-j]) {
+                return distinctCharacters - j;
+            }
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     std::vector<std::string> lines = readFile(argc, argv);
 
     for (auto& j : lines) {
         const char* buffer = j.c_str();
-        for (int i = 3; i < j.length();) {
-            if (buffer[i] == buffer[i-1]) {
-                i += 3;
-            } else if (buffer[i] == buffer[i-2]) {
-                i += 2;
-            } else if (buffer[i-1] == buffer[i-2]) {
-                i += 2;
-            } else if (buffer[i] == buffer[i-3]) {
-                i += 1;
-            } else if (buffer[i-1] == buffer[i-3]) {
-                i += 1;
-            } else if (buffer[i-2] == buffer[i-3]) {
-                i += 1;
+        for (int i = distinctCharacters - 1; i < j.length();) {
+            int stepCount = getStep(&buffer[i]);
+            if (stepCount) {
+                i += stepCount;
             } else {
                 std::cout << " First start of packet is: " << (i + 1) << std::endl;
                 break;
