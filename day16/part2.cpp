91f824e61017c371114e852fc19669829eb272e5
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2022-12-18 09:54:51 +0000

    Day 16 part 2

--- day16/part2.cpp
+++ day16/part2.cpp
@@ -22,6 +22,10 @@ struct char2 {
     char2(const std::string& str) : first(str[0]), second(str[1]) {
     }
 
+    friend std::ostream& operator<<(std::ostream& output, const char2& p) {
+        return output << '\'' << p.first << p.second << '\'';
+    }
+
     friend std::istream& operator>>(std::istream& input, char2* p) {
         char a, b;
         input >> a >> b;
@@ -47,23 +51,58 @@ struct char2 {
 std::map<char2, std::map<char2, int>> vals;
 std::map<char2, int> flows;
 
-int calc(const int n, const char2 pos, std::map<char2, bool>& toDo) {
+int calc(const int n, const char2 pos, const char2 posElephant, const int togo, const int togoElephant, std::map<char2, bool>& toDo) {
     if (n >= 30) {
         return 0;
     }
-    const int currentNodeValue = (30 - n) * flows[pos];
-    if (toDo.empty()) {
+
+    if (n <= 10) {
+        std::cout << "n == " << n << " try " << pos << " and " << posElephant << std::endl;
+    }
+
+    int currentNodeValue = 0;
+    const bool meFinished = togo == 0;
+    const bool elephantFinished = togoElephant == 0;
+    if (meFinished) {
+        currentNodeValue += (30 - n) * flows[pos];
+    } else if (elephantFinished) {
+        currentNodeValue += (30 - n) * flows[posElephant];
+    }
+
+    std::vector<char2> currentToDo;
+    for (auto& next : toDo) {
+        if (next.second) {
+            currentToDo.push_back(next.first);
+        }
+    }
+    if (currentToDo.empty() && meFinished && elephantFinished) {
         return currentNodeValue;
     } else {
         int max = 0;
-        for (auto next = toDo.begin(); next != toDo.end(); next++) {
-            if (next->second) {
-                int pathLength = (next->first < pos) ? vals[next->first][pos] : vals[pos][next->first];
-                next->second = false;
-                int val = calc(n + pathLength + 1, next->first, toDo);
-                next->second = true;
+        if (meFinished) {
+            for (auto& next : currentToDo) {
+                int pathLength = vals[pos][next];
+                int stepSize = std::max(std::min(togoElephant, pathLength + 1), 0);
+                toDo[next] = false;
+                int val = calc(n + stepSize, next, posElephant, pathLength + 1 - stepSize, togoElephant - stepSize, toDo);
+                toDo[next] = true;
                 max = std::max(max, val);
             }
+            int val = calc(n + togoElephant, pos, posElephant, 10000, 0, toDo);
+            max = std::max(max, val);
+        } else if (elephantFinished) {
+            for (auto& next : currentToDo) {
+                int pathLength = vals[posElephant][next];
+                int stepSize = std::max(std::min(togo, pathLength + 1), 0);
+                toDo[next] = false;
+                int val = calc(n + stepSize, pos, next, togo - stepSize, pathLength + 1 - stepSize, toDo);
+                toDo[next] = true;
+                max = std::max(max, val);
+            }
+            int val = calc(n + togo, pos, posElephant, 0, 10000, toDo);
+            max = std::max(max, val);
+        } else {
+            throw "Dit is foute boel!";
         }
         return currentNodeValue + max;
     }
@@ -132,7 +171,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    int value = calc(0, std::string("AA"), toDo);
+    int value = calc(4, char2("AA"), char2("AA"), 0, 0, toDo);
 
     std::cout << "Max value: " << value << std::endl;
 
