3eeb1a11b5f9e490d1042a98bcfd2bc05b9aebd6
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2022-12-18 21:39:38 +0000

    Day 16 part 1

--- day17/part1.cpp
+++ day17/part1.cpp
@@ -36,7 +36,7 @@ template<typename T> struct repeating_iterator {
     T current = begin;
 
     repeating_iterator& operator++(int) {
-        if (current++ == end) {
+        if (++current == end) {
             current = begin;
         }
         return *this;
@@ -52,29 +52,41 @@ template<typename T> struct repeating_iterator {
 };
 
 int moveRocks(const std::set<rock>& rocks, const formation& form, int x, int y, char move) {
+    std::cout << move << ' ';
     const int dirX = move == '<' ? -1 : (move == '>' ? 1 : 0);
     if (dirX) {
-        if ((x + dirX) > 7 || (x + dirX) < 0) {
+        if ((x + dirX + form.width) > 7 || (x + dirX) < 0) {
+            std::cout << '!' << ' ' << x << std::endl;
             return x;
         } else {
             for (auto& j : form.rocks) {
                 if (rocks.find(j + rock({x + dirX, y})) != rocks.end()) {
+                    std::cout << '!' << ' ' << x << std::endl;
                     return x;
                 }
             }
+            std::cout << move << ' ' << (x + dirX) << std::endl;
             return x + dirX;
         }
     }
     const int dirY = move == 'v' ? -1 : 0;
     if (dirY) {
-        if ((y + dirY) < 0) {
+        if ((y + dirY) <= 0) {
+            std::cout << '#' << ' ' << y << std::endl;
             return y;
         } else {
+            const int maxY = rocks.empty() ? 0 : rocks.rbegin()->y;
+            if (y + dirY > maxY) {
+                std::cout << move << ' ' << (y + dirY) << std::endl;
+                return y + dirY;
+            }
             for (auto& j : form.rocks) {
                 if (rocks.find(j + rock({x, y + dirY})) != rocks.end()) {
+                    std::cout << '#' << std::endl;
                     return y;
                 }
             }
+            std::cout << move << ' ' << (y + dirY) << std::endl;
             return y + dirY;
         }
     }
@@ -89,7 +101,7 @@ int main(int argc, char* argv[]) {
         {3, {{1, 0}, {0, 1}, {1, 1}, {2, 1}, {1, 2}}},
         {3, {{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}}},
         {1, {{0, 0}, {0, 1}, {0, 2}, {0, 3}}},
-        {2, {{0, 0}, {1, 0}, {1, 0}, {1, 1}}}
+        {2, {{0, 0}, {1, 0}, {0, 1}, {1, 1}}}
     };
 
     repeating_iterator<const formation*> current_formation = {&formations[0], &formations[5]};
@@ -97,23 +109,42 @@ int main(int argc, char* argv[]) {
 
     int currentHeight = 0;
     std::set<rock> rocks;
-    for (int i = 0; i < 2023; i++) {
-        int y = std::max_element(rocks.begin(), rocks.end())->y + 3;
+    for (int i = 0; i < 2022; i++) {
+        int y = rocks.empty() ? 4 : rocks.rbegin()->y + 4;
         int x = 2;
 
         while (true) {
-            x = moveRocks(rocks, *current_formation, x, y, *current_jet++);
+            std::cout << "x " << x << ' ';
+            x = moveRocks(rocks, *current_formation, x, y, *current_jet);
+            current_jet++;
 
-            if (moveRocks(rocks, *current_formation, x, y, 'v') == y) {
+            std::cout << "y " << y << ' ';
+            const int ytemp = moveRocks(rocks, *current_formation, x, y, 'v');
+            if (y == ytemp) {
                 for (auto& r : current_formation->rocks) {
                     rocks.insert(r + rock({x, y}));
                 }
                 break;
             } else {
-                y++;
+                y = ytemp;
             }
         }
+        current_formation++;
     }
+
+    std::cout << std::endl;
+    for (int y = rocks.rbegin()->y; y >= 1; y--) {
+        for (int x = 0; x < 7; x++) {
+            if (rocks.find({x, y}) == rocks.end()) {
+                std::cout << '.';
+            } else {
+                std::cout << '#';
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Tower is " << rocks.rbegin()->y << " units tall!" << std::endl;
 
     std::cout << std::endl << std::endl << std::endl;
 }
